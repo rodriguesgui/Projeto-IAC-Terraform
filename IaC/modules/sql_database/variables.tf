5a1f6029ef15ce8d956434e3b3d87cb00ce56c37
Guilherme Rodrigues de Quadros <guilhermeddq@gmail.com>
2026-03-28 17:26:12 -0300

    feat: variable sql_database p1

---
+++ IaC/modules/sql_database/variables.tf
@@ -0,0 +1,26 @@
+variable "resource_group_name" {
+    description = "Nome do Resource Group onde o sql vai ser criado"
+    type = string
+}
+
+variable "location" {
+    description = "Localização do Resource Group onde o sql vai ser criado"
+    type = string
+}
+
+variable "environment" {
+    description = "Ambiente do Resource Group onde o sql vai ser criado"
+    type = string
+}
+
+variable "sql_admin_login" {
+    description = "Login do administrador do SQL Database"
+    type = string
+    sensitive = true
+}
+
+variable "sql_admin_password" {
+    description = "Senha do administrador do SQL Database"
+    type = string
+    sensitive = true
+}
