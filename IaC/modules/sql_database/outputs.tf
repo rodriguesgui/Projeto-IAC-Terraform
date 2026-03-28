726e4b5b805b62bc3e13fc34537940c8ad7c3280
Guilherme Rodrigues de Quadros <guilhermeddq@gmail.com>
2026-03-28 17:45:14 -0300

    feat: adicionand outputs sql database pt1

---
+++ IaC/modules/sql_database/outputs.tf
@@ -0,0 +1,14 @@
+output "sql_server_name" {
+    description = "Nome do SQL Server criado"
+    value = azurerm_mssql_server.main.name
+}
+
+output "sql_database_id" {
+    description = "ID do SQL Database criado"
+    value = azurerm_mssql_database.main.id
+}
+
+output "sql_database_name" {
+    description = "Nome do SQL Database criado"
+    value = azurerm_mssql_database.main.name
+}
