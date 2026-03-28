30de4d7bc97a5170ac3138515615e9e3c5884752
Guilherme Rodrigues de Quadros <guilhermeddq@gmail.com>
2026-03-28 17:22:56 -0300

    feat: adicionando outputs pt1

---
+++ IaC/outputs.tf
@@ -0,0 +1,14 @@
+output "resource_group_name" {
+    description = "Nome do Resouce Group criado"
+    value = azurerm_resource_group.main.name
+}
+
+output "location" {
+    description = "Localização do Resource Group criado"
+    value = azurerm_resource_group.main.location
+}
+
+output "enviroment" {
+    description = "Ambiente do Resource Group criado"
+    value = var.environment
+}
