6cdbfc18c057ee187d2d3acb7b7e0da0e2a2ed2c
Guilherme Rodrigues de Quadros <guilhermeddq@gmail.com>
2026-03-29 19:36:48 -0300

    corrigindo beckend e yml

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -1,10 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate12174"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-    use_oidc             = false
-  }
+  backend "azurerm" {}
 }
-
